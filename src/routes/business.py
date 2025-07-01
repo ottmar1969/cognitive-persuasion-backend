488e4f9f34b384000cbd7c44643d14473b24442e
ottmar1969 <158744549+ottmar1969@users.noreply.github.com>
2025-07-01 13:52:30 +0800

    Update business.py

--- src/routes/business.py
+++ src/routes/business.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from src.models.user import db, User, BusinessType
-from src.utils.init_data import get_predefined_business_types
+from models.user import db, User, BusinessType
+from utils.init_data import get_predefined_business_types
 
 business_bp = Blueprint('business', __name__)
 
