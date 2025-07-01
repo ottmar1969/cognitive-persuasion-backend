00aaaa1a1d812447132907995030b75f8e3645e0
ottmar1969 <158744549+ottmar1969@users.noreply.github.com>
2025-07-01 13:53:22 +0800

    Update business_simple.py

--- src/routes/business_simple.py
+++ src/routes/business_simple.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from src.models.user_simple import db, User, BusinessType
+from models.user_simple import db, User, BusinessType
 
 business_bp = Blueprint('business', __name__)
 
